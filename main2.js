6ec1a60c4e34a6223d00bb20fd59d75b6ae105eb
Muhammed Vasil <mvasil04@gmail.com>
2026-02-02 01:27:53 +0530

    Update main2.js

--- main2.js
+++ main2.js
@@ -102,7 +102,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const wedate = document.getElementById("wedate");
         const btns = document.getElementById("btns");
         btns.style.display = "none";
-        wedate.innerText = "We are each other's valentine now. I love you cutie. ❤️😘";
+        wedate.innerText = "We are officially Valentines now 💖 I love you so much, Noora 🌸";
 
         // Open index1.html in a new tab
         window.open("index1.html", "_blank");
